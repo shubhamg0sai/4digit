f9145d1675022d6d6c7af642e62cb086004f7337
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-01-10 11:37:45 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash 
+echo "----------------------------------------------------------------------------- ""
+echo -e "\e[1;31mThis Script is ShuBhamg0sain by now start"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+echo   " we are anynomous"               
+ echo   " we are hackers "                                                                    
+echo   "####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+
+apt-get update -y                      #update in progress
+apt install python -y           #installation......
+apt instll git -y
+apt install python -y
+apt install python2 -y
+apt install php -y
+apt install openssh -y
+git clone https://github.com/ShuBhamg0sain/adb-server
+cd adb-server
+chmod +x install-adb-server.sh
+bash install-adb-server.sh
+ls
+cd
+git clone https://github.com/ShuBhamg0sain/4digit-phone-pin
+cd 4digit-phone-pin
+chmod +x 4digipincracker
+bash 4digipincracker
+
+echo  "#####################################"
+echo   " u can now hack anything      "               
+ echo   " completing wait 3 2 1 "                                                                    
+echo   "####################################"
+
+
+echo  "#####################################"
+echo   "anynomous "               
+ echo   " join us on facebook https://m.facebook.com/shubham.gosain.980 "                                                                   echo   "####################################"
+
+
+echo " "                                                                   
+echo   "####################################"
+
+                                                            echo   " "
+                                                            
+echo  "#####################################"
+echo   " we are hackers "               
+                                                                  echo   "####################################"
+
+echo  "#####################################"
+               
+echo   " processing wait......
+"                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then u downloaded success fully follow me Instagram https://www.instagram.com/shubham_g0sain/?hl=en "
+
+echo " ??? now rock and hack ??? "
+
+exit
